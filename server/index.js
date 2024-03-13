825a85df8fbe12bd545f9a2bf6c0c1ce9a5574a7
AmayameenuNibi <136682099+AmayameenuNibi@users.noreply.github.com>
2024-03-13 15:43:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,11 +33,11 @@ const dirname = path.resolve();
 app.use("/uploads", express.static('uploads'))
 
 app.use('/api/users', userRoutes);
-app.use('/api/countries', countryRoutes);
-app.use('/api/sites', siteRoutes);
-app.use('/api/categories', categoryRoutes);
-app.use('/api/companies', companyRoutes);
-app.use("/auth", authRoutes);
+// app.use('/api/countries', countryRoutes);
+// app.use('/api/sites', siteRoutes);
+// app.use('/api/categories', categoryRoutes);
+// app.use('/api/companies', companyRoutes);
+// app.use("/auth", authRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
